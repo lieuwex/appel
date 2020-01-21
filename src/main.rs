29824f61775257150568e39464896bd0a9dfc79c
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2020-01-21 19:12:39 +0100

    more interesting example combining parser and executor

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
 
     let mut exec = Executor::new();
 
-    let vec = Expr::Vector(vec![ Expr::Atom(Atom::Rat(Ratio::from_f32(5.0).unwrap())), Expr::Atom(Atom::Rat(Ratio::from_f32(10.0).unwrap())) ]);
+    let vec = Expr::Vector(vec![ Expr::Atom(parser::parse("1.2").unwrap()), Expr::Atom(parser::parse("5.4").unwrap()) ]);
     let res = exec.execute_expr(Expr::Fold(BinOp::Add, Box::new(vec)));
 
     println!("{:?}", res);
