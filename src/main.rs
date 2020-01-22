acaa1723537a350571c208c9363606e1cfea75a4
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2020-01-22 14:10:33 +0100

    improve example in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,44 @@
 mod ast;
-mod parser;
 mod executor;
+mod parser;
 
 use num_traits::cast::FromPrimitive;
 
-use crate::executor::*;
 use crate::ast::*;
+use crate::executor::*;
 
 fn main() {
     println!("{:?}", parser::parse("1.2").unwrap());
 
     let mut exec = Executor::new();
 
-    let vec = Expr::Vector(vec![ Expr::Atom(parser::parse("1.2").unwrap()), Expr::Atom(parser::parse("5.4").unwrap()) ]);
-    let res = exec.execute_expr(Expr::Fold(BinOp::Add, Box::new(vec)));
+    // add some numbers
+    let res = exec.execute_expr(Expr::Fold(
+        BinOp::Add,
+        Box::new(Expr::Vector(vec![
+            parser::parse("1.5").unwrap(),
+            parser::parse("3.5").unwrap(),
+            parser::parse("5").unwrap(),
+        ])),
+    ));
+    println!("{:?}", res);
 
+    // make function
+    let res = exec.execute_expr(Expr::FunDeclare(
+        String::from("perc"),
+        vec![String::from("a")],
+        Box::new(Expr::Binary(
+            Box::new(Expr::Atom(Atom::Ref(String::from("a")))),
+            BinOp::Div,
+            Box::new(Expr::Atom(Atom::Rat(Ratio::from_u64(100).unwrap()))),
+        )),
+    ));
+    println!("{:?}", res);
+
+    // call function
+    let res = exec.execute_expr(Expr::Vector(vec![
+        Expr::Atom(Atom::Ref(String::from("perc"))),
+        Expr::Atom(Atom::Rat(Ratio::from_f64(12.5).unwrap())),
+    ]));
     println!("{:?}", res);
 }
