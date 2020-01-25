d984931d8766317dcd69dd2ce9e731deff12eb7e
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2020-01-25 14:24:35 +0100

    name parsers

--- src/parser.rs
+++ src/parser.rs
@@ -8,6 +8,7 @@ type Parser<'a, T> = pom::parser::Parser<'a, char, T>;
 fn left_recurse<'a, E: 'a, O: 'a>(
     atom_p: impl Fn() -> Parser<'a, E>,
     op_p: impl Fn() -> Parser<'a, O>,
+    name: &'a str,
     combine: impl Fn(E, O, E) -> E + 'a
 ) -> Parser<'a, E> {
     (atom_p() + (op_p() + atom_p()).repeat(0..)).map(move |(mut expr, v)| {
@@ -15,7 +16,7 @@ fn left_recurse<'a, E: 'a, O: 'a>(
             expr = combine(expr, op, expr2);
         }
         expr
-    })
+    }).name(name)
 }
 
 fn binary_op<'a>() -> Parser<'a, BinOp> {
@@ -28,15 +29,15 @@ fn binary_op<'a>() -> Parser<'a, BinOp> {
 }
 
 fn whitespace<'a>() -> Parser<'a, ()> {
-    is_a(|c: char| c.is_ascii_whitespace()).repeat(0..).discard()
+    is_a(|c: char| c.is_ascii_whitespace()).repeat(0..).discard().name("whitespace")
 }
 
 fn symbol<'a, T: 'a>(p: Parser<'a, T>) -> Parser<'a, T> {
-    whitespace() * p
+    (whitespace() * p).name("symbol")
 }
 
 fn operator<'a>(text: &'static str) -> Parser<'a, ()> {
-    tag(text).discard()
+    tag(text).discard().name("op")
 }
 
 fn integer_decimal(s: &[char]) -> Result<BigUint, num_bigint::ParseBigIntError> {
@@ -79,11 +80,11 @@ fn p_hexdigit<'a>() -> Parser<'a, char> {
     is_a(|c: char| {
         let c = c.to_ascii_lowercase();
         ('0' <= c && c <= '9') || ('a' <= c && c <= 'f')
-    })
+    }).name("hex")
 }
 
 fn p_digit<'a>() -> Parser<'a, char> {
-    is_a(|c| '0' <= c && c <= '9')
+    is_a(|c| '0' <= c && c <= '9').name("digit")
 }
 
 fn p_posexp<'a>() -> Parser<'a, BigUint> {
@@ -94,7 +95,7 @@ fn p_posexp<'a>() -> Parser<'a, BigUint> {
 fn negative<'a, T: 'static>(p: Parser<'a, T>) -> Parser<'a, T>
         where T: std::ops::Neg<Output = T> {
     let p_sign = sym('-').opt().map(|o| o.is_some());
-    (p_sign + p).map(|(s, v)| if s { -v } else { v })
+    (p_sign + p).map(|(s, v)| if s { -v } else { v }).name("neg")
 }
 
 fn p_exp<'a>() -> Parser<'a, BigInt> {
@@ -107,7 +108,7 @@ fn p_int<'a>() -> Parser<'a, Atom> {
                 .collect().convert(integer_hex)
             | (p_digit().repeat(1..).collect().convert(integer_decimal) + p_posexp())
                 .map(|(b, e)| b * BigUint::from(10u32).pow(e));
-    negative(p.map(BigInt::from)).map(Ratio::from).map(Atom::Rat)
+    negative(p.map(BigInt::from)).map(Ratio::from).map(Atom::Rat).name("int")
 }
 
 fn p_float<'a>() -> Parser<'a, Atom> {
@@ -132,32 +133,32 @@ fn p_float<'a>() -> Parser<'a, Atom> {
         }) as Result<Ratio, num_bigint::ParseBigIntError>
     });
 
-    negative(p_ratio).map(Atom::Rat)
+    negative(p_ratio).map(Atom::Rat).name("float")
 }
 
 fn p_varname<'a>() -> Parser<'a, String> {
     (
         is_a(|c: char| c.is_alphabetic() || c == '_')
         + is_a(|c: char| c.is_alphanumeric() || c == '_').repeat(0..)
-    ).collect().map(|s| s.iter().collect::<String>())
+    ).collect().map(|s| s.iter().collect::<String>()).name("variable name")
 }
 
 fn p_var<'a>() -> Parser<'a, Atom> {
-    p_varname().map(Atom::Ref)
+    p_varname().map(Atom::Ref).name("variable")
 }
 
 fn p_atom<'a>() -> Parser<'a, Atom> {
-    symbol(p_float() | p_int() | p_var())
+    symbol(p_float() | p_int() | p_var()).name("atom")
 }
 
 /// Parenthesised expression or plain atom
 fn p_expr_0<'a>() -> Parser<'a, Expr> {
-    (symbol(operator("(")) * call(p_expr) - symbol(operator(")"))) | p_atom().map(Expr::Atom)
+    (symbol(operator("(")) * call(p_expr) - symbol(operator(")"))).name("paren") | p_atom().map(Expr::Atom)
 }
 
 /// Vector of atom-like things
 fn p_expr_1<'a>() -> Parser<'a, Expr> {
-    p_expr_0().repeat(1..).map(|v| Expr::Vector(v.to_vec()))
+    p_expr_0().repeat(1..).map(|v| Expr::Vector(v.to_vec())).name("vector")
 }
 
 /// Unary operators on a vector
@@ -167,7 +168,7 @@ fn p_expr_2<'a>() -> Parser<'a, Expr> {
             operator("+").map(|_| UnOp::Id)
             | operator("-").map(|_| UnOp::Neg)
             | operator("!").map(|_| UnOp::Not)
-        )
+        ).name("unary")
     }
 
     (p_unary().repeat(0..) + p_expr_1()).map(|(ops, mut ex)| {
@@ -181,7 +182,7 @@ fn p_expr_2<'a>() -> Parser<'a, Expr> {
 /// Power (**) of unary'd vector
 fn p_expr_3<'a>() -> Parser<'a, Expr> {
     let op_p = || symbol(operator("**")).map(|_| BinOp::Pow);
-    left_recurse(p_expr_2, op_p, |e1, op, e2| Expr::Binary(Box::new(e1), op, Box::new(e2)))
+    left_recurse(p_expr_2, op_p, "power", |e1, op, e2| Expr::Binary(Box::new(e1), op, Box::new(e2)))
 }
 
 /// Product (*, /, %) of powers
@@ -189,14 +190,14 @@ fn p_expr_4<'a>() -> Parser<'a, Expr> {
     let op_p = || symbol(operator("*")).map(|_| BinOp::Mul)
                     | symbol(operator("/")).map(|_| BinOp::Div)
                     | symbol(operator("%")).map(|_| BinOp::Mod);
-    left_recurse(p_expr_3, op_p, |e1, op, e2| Expr::Binary(Box::new(e1), op, Box::new(e2)))
+    left_recurse(p_expr_3, op_p, "product", |e1, op, e2| Expr::Binary(Box::new(e1), op, Box::new(e2)))
 }
 
 /// Sum (+, -) of products
 fn p_expr_5<'a>() -> Parser<'a, Expr> {
     let op_p = || symbol(operator("+")).map(|_| BinOp::Add)
                     | symbol(operator("-")).map(|_| BinOp::Sub);
-    left_recurse(p_expr_4, op_p, |e1, op, e2| Expr::Binary(Box::new(e1), op, Box::new(e2)))
+    left_recurse(p_expr_4, op_p, "sum", |e1, op, e2| Expr::Binary(Box::new(e1), op, Box::new(e2)))
 }
 
 /// Fold
@@ -204,7 +205,7 @@ fn p_expr_6<'a>() -> Parser<'a, Expr> {
     let op_p = || binary_op().map(FoldOp::BinOp) | p_varname().map(FoldOp::FunctionRef);
     let fold = (op_p() - symbol(operator("//")) + call(p_expr)).map(|(op, expr)| Expr::Fold(op, Box::new(expr)));
 
-    fold | p_expr_5()
+    (fold).name("fold") | p_expr_5()
 }
 
 // fn p_expr_fn<'a>() -> Parser<'a, Expr> {
