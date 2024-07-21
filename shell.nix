0ec2e2a9c10f73cc5178015bf4fab86c1291fc5c
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2024-07-21 23:21:24 +0200

    add lorri information

---
+++ shell.nix
@@ -0,0 +1,14 @@
+with (import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/release-24.05.tar.gz") {});
+
+pkgs.mkShell {
+	buildInputs = [
+		rust-bin.stable.latest.complete
+		cargo-outdated
+		cargo-show-asm
+		cargo-flamegraph
+
+		openssl
+		pkg-config
+		gnum4
+	];
+}
